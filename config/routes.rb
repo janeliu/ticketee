e8518165ec1dd24cbc42fb9d61de375c908e77cc
Jane Liu <janeliu94@gmail.com>
2013-07-16 21:20:40 -0700

    Add feature to ensure Devise signup is always working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Ticketee::Application.routes.draw do
 
+  devise_for :users
+
   root :to => "projects#index"
 
   Ticketee::Application.routes.draw do
+  devise_for :users
+
     resources :projects do
       resources :tickets
     end
